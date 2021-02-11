d3ac31d231e09dd81dac98bcf14bce8827edcc0b
Matthew Kennedy <m.kennedy@me.com>
2021-02-11 20:24:33 +0000

    Add select default attributes

--- src/elements/select.js
+++ src/elements/select.js
@@ -1,13 +1,32 @@
-function disableSelect (container) {
+export function selectDefaultAttributes (container) {
+  container.querySelectorAll('select').forEach(function (element) {
+    if (element.required === true) {
+      element.setAttribute('data-rc-required', true)
+    }
+
+    if (element.disabled === true) {
+      element.setAttribute('data-rc-disabled', true)
+    }
+  })
+}
+
+export function disableSelect (container) {
   container.querySelectorAll('select').forEach(function (element) {
     element.disabled = true
+    element.required = false
   })
 }
 
-function enableSelect (container) {
+export function enableSelect (container) {
   container.querySelectorAll('select').forEach(function (element) {
     element.disabled = false
+
+    if (element.hasAttribute('data-rc-required')) {
+      element.required = true
+    }
+
+    if (element.hasAttribute('data-rc-disabled')) {
+      element.disabled = true
+    }
   })
 }
-
-export { disableSelect, enableSelect }
--- src/utils/state.js
+++ src/utils/state.js
@@ -1,5 +1,6 @@
 import { inputDefaultAttributes } from '../elements/input.js'
 import { textareaDefaultAttributes } from '../elements/textarea.js'
+import { selectDefaultAttributes } from '../elements/select.js'
 
 export function recordElState (rcPanelsGroup) {
   const group = document.querySelector(`[data-rc-panel-group=${rcPanelsGroup}]`)
@@ -7,5 +8,6 @@ export function recordElState (rcPanelsGroup) {
   group.querySelectorAll('[data-rc-panel-uid]').forEach(function (elem) {
     inputDefaultAttributes(elem)
     textareaDefaultAttributes(elem)
+    selectDefaultAttributes(elem)
   })
 }
