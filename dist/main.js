2c7161a14e64148c22036081e453cb42bd4b1717
Matthew Kennedy <m.kennedy@me.com>
2021-02-11 15:50:12 +0000

    Update

--- dist/main.js
+++ dist/main.js
@@ -7,9 +7,87 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-eval("var radioButtonsContainer = '[data-radio-control]';\ndocument.addEventListener('DOMContentLoaded', function () {\n  if (document.querySelector(radioButtonsContainer)) {\n    document.querySelectorAll(radioButtonsContainer).forEach(function (elem) {\n      var radioControlEl = document.querySelector(\"[data-radio-control=\".concat(elem.dataset.radioControl, \"]\"));\n      initiateRcPanels(radioControlEl);\n      monitorRadiosForChange(radioControlEl);\n    });\n  }\n});\n\nfunction initiateRcPanels(radioControlEl) {\n  var selectedRadioValue = radioControlEl.querySelector('input[type=radio]:checked').value;\n  hideRcPanels(radioControlEl.dataset.radioControl);\n  activatePanel(\"[data-rc-panel-uid=\".concat(radioControlEl.dataset.radioControl + '_' + selectedRadioValue, \"]\"));\n}\n\nfunction monitorRadiosForChange(radioControlEl) {\n  radioControlEl.querySelectorAll('input[type=radio]').forEach(function (elem) {\n    elem.addEventListener('change', function (event) {\n      var item = event.target.value;\n      hideRcPanels(radioControlEl.dataset.radioControl);\n      activatePanel(\"[data-rc-panel-uid=\".concat(radioControlEl.dataset.radioControl + '_' + item, \"]\"));\n    });\n  });\n}\n\nfunction hideRcPanels(rcPanelsGroup) {\n  var group = document.querySelector(\"[data-rc-panel-group=\".concat(rcPanelsGroup, \"]\"));\n  group.querySelectorAll('[data-rc-panel-uid]').forEach(function (elem) {\n    elem.classList.remove('d-block');\n    elem.classList.add('d-none');\n  });\n}\n\nfunction activatePanel(panel) {\n  var active = document.querySelector(panel);\n  active.classList.remove('d-none');\n  active.classList.add('d-block');\n}\n\n//# sourceURL=webpack://radio-control-js/./src/index.js?");
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _utils_visibility_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils/visibility.js */ \"./src/utils/visibility.js\");\n\nvar radioButtonsContainer = '[data-radio-control]';\ndocument.addEventListener('DOMContentLoaded', function () {\n  if (document.querySelector(radioButtonsContainer)) {\n    document.querySelectorAll(radioButtonsContainer).forEach(function (elem) {\n      var radioControlEl = document.querySelector(\"[data-radio-control=\".concat(elem.dataset.radioControl, \"]\"));\n      initiateRcPanels(radioControlEl);\n      monitorRadiosForChange(radioControlEl);\n    });\n  }\n});\n\nfunction initiateRcPanels(radioControlEl) {\n  var selectedRadioValue = radioControlEl.querySelector('input[type=radio]:checked').value;\n  (0,_utils_visibility_js__WEBPACK_IMPORTED_MODULE_0__.hideRcPanels)(radioControlEl.dataset.radioControl);\n  (0,_utils_visibility_js__WEBPACK_IMPORTED_MODULE_0__.activatePanel)(\"[data-rc-panel-uid=\".concat(radioControlEl.dataset.radioControl + '_' + selectedRadioValue, \"]\"));\n}\n\nfunction monitorRadiosForChange(radioControlEl) {\n  radioControlEl.querySelectorAll('input[type=radio]').forEach(function (elem) {\n    elem.addEventListener('change', function (event) {\n      var item = event.target.value;\n      (0,_utils_visibility_js__WEBPACK_IMPORTED_MODULE_0__.hideRcPanels)(radioControlEl.dataset.radioControl);\n      (0,_utils_visibility_js__WEBPACK_IMPORTED_MODULE_0__.activatePanel)(\"[data-rc-panel-uid=\".concat(radioControlEl.dataset.radioControl + '_' + item, \"]\"));\n    });\n  });\n}\n\n//# sourceURL=webpack://radio-control-js/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/utils/visibility.js":
+/*!*********************************!*\
+  !*** ./src/utils/visibility.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"hideRcPanels\": () => (/* binding */ hideRcPanels),\n/* harmony export */   \"activatePanel\": () => (/* binding */ activatePanel)\n/* harmony export */ });\nfunction hideRcPanels(rcPanelsGroup) {\n  var group = document.querySelector(\"[data-rc-panel-group=\".concat(rcPanelsGroup, \"]\"));\n  group.querySelectorAll('[data-rc-panel-uid]').forEach(function (elem) {\n    elem.classList.remove('d-block');\n    elem.classList.add('d-none');\n  });\n}\n\nfunction activatePanel(panel) {\n  var active = document.querySelector(panel);\n  active.classList.remove('d-none');\n  active.classList.add('d-block');\n}\n\n\n\n//# sourceURL=webpack://radio-control-js/./src/utils/visibility.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { hideRcPanels, activatePanel } from './utils/visibility.js'
+
 var radioButtonsContainer = '[data-radio-control]'
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -27,20 +29,4 @@ function monitorRadiosForChange (radioControlEl) {
       activatePanel(`[data-rc-panel-uid=${radioControlEl.dataset.radioControl + '_' + item}]`)
     })
   })
-}
-
-function hideRcPanels (rcPanelsGroup) {
-  var group = document.querySelector(`[data-rc-panel-group=${rcPanelsGroup}]`)
-
-  group.querySelectorAll('[data-rc-panel-uid]').forEach(function (elem) {
-    elem.classList.remove('d-block')
-    elem.classList.add('d-none')
-  })
-}
-
-function activatePanel (panel) {
-  var active = document.querySelector(panel)
-
-  active.classList.remove('d-none')
-  active.classList.add('d-block')
 }
