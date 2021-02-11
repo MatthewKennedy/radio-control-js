c8deb379fcac9f0aa13e9721e25bc2c10b54e53f
Matthew Kennedy <m.kennedy@me.com>
2021-02-11 20:12:15 +0000

    Add text field default attributes

--- src/elements/textarea.js
+++ src/elements/textarea.js
@@ -1,13 +1,32 @@
-function disableTextArea (container) {
+export function textareaDefaultAttributes (container) {
+  container.querySelectorAll('textarea').forEach(function (element) {
+    if (element.required === true) {
+      element.setAttribute('data-rc-required', true)
+    }
+
+    if (element.disabled === true) {
+      element.setAttribute('data-rc-disabled', true)
+    }
+  })
+}
+
+export function disableTextArea (container) {
   container.querySelectorAll('textarea').forEach(function (element) {
     element.disabled = true
+    element.required = false
   })
 }
 
-function enableTextArea (container) {
+export function enableTextArea (container) {
   container.querySelectorAll('textarea').forEach(function (element) {
     element.disabled = false
+
+    if (element.hasAttribute('data-rc-required')) {
+      element.required = true
+    }
+
+    if (element.hasAttribute('data-rc-disabled')) {
+      element.disabled = true
+    }
   })
 }
-
-export { disableTextArea, enableTextArea }
--- src/utils/state.js
+++ src/utils/state.js
@@ -1,9 +1,11 @@
 import { inputDefaultAttributes } from '../elements/input.js'
+import { textareaDefaultAttributes } from '../elements/textarea.js'
 
 export function recordElState (rcPanelsGroup) {
   const group = document.querySelector(`[data-rc-panel-group=${rcPanelsGroup}]`)
 
   group.querySelectorAll('[data-rc-panel-uid]').forEach(function (elem) {
     inputDefaultAttributes(elem)
+    textareaDefaultAttributes(elem)
   })
 }
