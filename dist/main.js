af94f08466a7ce686e77bedd7a3f59aabc5d6ca7
Matthew Kennedy <m.kennedy@me.com>
2021-07-03 15:30:13 +0100

    update

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,126 @@
-(()=>{"use strict";function e(e,t){for(var a=0;a<t.length;a++){var n=t[a];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(e,n.key,n)}}var t=function(){function t(){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,t)}var a,n;return a=t,(n=[{key:"recordDefaults",value:function(e){!0===e.required&&e.setAttribute("data-rc-required",!0),!0===e.disabled&&e.setAttribute("data-rc-disabled",!0),!0===e.checked&&e.setAttribute("data-rc-checked",!0),!0===e.selected&&e.setAttribute("data-rc-selected",!0)}},{key:"disable",value:function(e){e.disabled=!0,e.required=!1}},{key:"enable",value:function(e){e.disabled=!1,e.hasAttribute("data-rc-required")&&(e.required=!0),e.hasAttribute("data-rc-disabled")&&(e.disabled=!0)}}])&&e(a.prototype,n),t}();function a(e,t){for(var a=0;a<t.length;a++){var n=t[a];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(e,n.key,n)}}var n="input, select, button, fieldset, textarea, option",r=function(){function e(){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,e)}var r,o;return r=e,(o=[{key:"disable",value:function(e){e.removeAttribute("data-rc-active"),e.querySelectorAll(n).forEach((function(e){(new t).disable(e)}))}},{key:"enable",value:function(e){e.setAttribute("data-rc-active",""),e.querySelectorAll(n).forEach((function(e){(new t).enable(e)}))}}])&&a(r.prototype,o),e}();function o(e,t){for(var a=0;a<t.length;a++){var n=t[a];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(e,n.key,n)}}var c=function(){function e(){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,e)}var t,a;return t=e,(a=[{key:"monitorForChange",value:function(e){e.querySelectorAll("input[type=radio]").forEach((function(t){t.addEventListener("change",(function(t){var a=t.target.value,n=new u;n.deactivate(e.dataset.radioControl),n.activate("[data-rc-panel-uid=".concat(e.dataset.radioControl+"_"+a,"]"))}))}))}},{key:"handleNestedRcRadios",value:function(e){var t=e.querySelector("input[type=radio]:checked"),a=e.querySelector("input[type=radio][data-rc-checked=true]");e.hasAttribute("data-rc-no-reset-nested-radios")?(new u).initiate(e):(t.checked=!1,a.click())}}])&&o(t.prototype,a),e}();function i(e,t){for(var a=0;a<t.length;a++){var n=t[a];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(e,n.key,n)}}var u=function(){function e(){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,e)}var t,a;return t=e,(a=[{key:"initiate",value:function(e){var t=e.querySelector("input[type=radio]:checked").value,a="[data-rc-panel-uid=".concat(e.dataset.radioControl+"_"+t,"]");this.deactivate(e.dataset.radioControl),this.activate(a)}},{key:"deactivate",value:function(e){var t=document.querySelector("[data-rc-panel-group=".concat(e,"]"));if(!t)return console.warn("Wrap your ".concat(e,' pannels with a block-level element, and add the attribute: [data-rc-panel-group="').concat(e,'"]'));t.querySelectorAll("[data-rc-panel-uid]").forEach((function(e){(new r).disable(e)}))}},{key:"activate",value:function(e){var t=document.querySelector(e);if(!t)return console.warn('There is no panel in your DOM with the attribute: "'.concat(e,'"'));(new r).enable(t),t.querySelectorAll("[data-radio-control]").forEach((function(e){(new c).handleNestedRcRadios(e)}))}}])&&i(t.prototype,a),e}(),l="[data-radio-control]";document.addEventListener("DOMContentLoaded",(function(){document.querySelector(l)&&(document.querySelectorAll("[data-rc-panel-group]").forEach((function(e){e.querySelectorAll("input, select, button, fieldset, textarea, option").forEach((function(e){(new t).recordDefaults(e)}))})),document.querySelectorAll(l).forEach((function(e){var t=document.querySelector("[data-radio-control=".concat(e.dataset.radioControl,"]")),a=new u,n=new c;a.initiate(t),n.monitorForChange(t)})))}))})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _utils_state_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils/state.js */ \"./src/utils/state.js\");\n/* harmony import */ var _utils_form_input_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./utils/form_input.js */ \"./src/utils/form_input.js\");\n/* harmony import */ var _utils_radio_controler_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./utils/radio_controler.js */ \"./src/utils/radio_controler.js\");\n\n\n\nvar FORM_INPUTS = 'input, select, button, fieldset, textarea, option';\nvar radioControlDataAttribute = '[data-radio-control]';\ndocument.addEventListener('DOMContentLoaded', function () {\n  if (!document.querySelector(radioControlDataAttribute)) return;\n  recordStateOfInputElements();\n  document.querySelectorAll(radioControlDataAttribute).forEach(function (elem) {\n    var radioControlParentEl = document.querySelector(\"[data-radio-control=\".concat(elem.dataset.radioControl, \"]\"));\n    var panelState = new _utils_state_js__WEBPACK_IMPORTED_MODULE_0__.State();\n    var radioControler = new _utils_radio_controler_js__WEBPACK_IMPORTED_MODULE_2__.RadioControler();\n    panelState.initiate(radioControlParentEl);\n    radioControler.monitorForChange(radioControlParentEl);\n  });\n});\n\nfunction recordStateOfInputElements() {\n  document.querySelectorAll('[data-rc-panel-group]').forEach(function (elem) {\n    elem.querySelectorAll(FORM_INPUTS).forEach(function (elem) {\n      var input = new _utils_form_input_js__WEBPACK_IMPORTED_MODULE_1__.FormInput();\n      input.recordDefaults(elem);\n    });\n  });\n}\n\n//# sourceURL=webpack://radio-control/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/utils/form_input.js":
+/*!*********************************!*\
+  !*** ./src/utils/form_input.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"FormInput\": () => (/* binding */ FormInput)\n/* harmony export */ });\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\nvar FormInput = /*#__PURE__*/function () {\n  function FormInput() {\n    _classCallCheck(this, FormInput);\n  }\n\n  _createClass(FormInput, [{\n    key: \"recordDefaults\",\n    value: function recordDefaults(el) {\n      if (el.required === true) el.setAttribute('data-rc-required', true);\n      if (el.disabled === true) el.setAttribute('data-rc-disabled', true);\n      if (el.checked === true) el.setAttribute('data-rc-checked', true);\n      if (el.selected === true) el.setAttribute('data-rc-selected', true);\n    }\n  }, {\n    key: \"disable\",\n    value: function disable(el) {\n      el.disabled = true;\n      el.required = false;\n    }\n  }, {\n    key: \"enable\",\n    value: function enable(el) {\n      el.disabled = false;\n      if (el.hasAttribute('data-rc-required')) el.required = true;\n      if (el.hasAttribute('data-rc-disabled')) el.disabled = true;\n    }\n  }]);\n\n  return FormInput;\n}();\n\n//# sourceURL=webpack://radio-control/./src/utils/form_input.js?");
+
+/***/ }),
+
+/***/ "./src/utils/panel.js":
+/*!****************************!*\
+  !*** ./src/utils/panel.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Panel\": () => (/* binding */ Panel)\n/* harmony export */ });\n/* harmony import */ var _form_input_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./form_input.js */ \"./src/utils/form_input.js\");\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\n\nvar FORM_INPUTS = 'input, select, button, fieldset, textarea, option';\nvar Panel = /*#__PURE__*/function () {\n  function Panel() {\n    _classCallCheck(this, Panel);\n  }\n\n  _createClass(Panel, [{\n    key: \"disable\",\n    value: function disable(el) {\n      el.removeAttribute('data-rc-active');\n      el.querySelectorAll(FORM_INPUTS).forEach(function (elem) {\n        var input = new _form_input_js__WEBPACK_IMPORTED_MODULE_0__.FormInput();\n        input.disable(elem);\n      });\n    }\n  }, {\n    key: \"enable\",\n    value: function enable(el) {\n      el.setAttribute('data-rc-active', '');\n      el.querySelectorAll(FORM_INPUTS).forEach(function (elem) {\n        var input = new _form_input_js__WEBPACK_IMPORTED_MODULE_0__.FormInput();\n        input.enable(elem);\n      });\n    }\n  }]);\n\n  return Panel;\n}();\n\n//# sourceURL=webpack://radio-control/./src/utils/panel.js?");
+
+/***/ }),
+
+/***/ "./src/utils/radio_controler.js":
+/*!**************************************!*\
+  !*** ./src/utils/radio_controler.js ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"RadioControler\": () => (/* binding */ RadioControler)\n/* harmony export */ });\n/* harmony import */ var _state_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./state.js */ \"./src/utils/state.js\");\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\n\nvar RadioControler = /*#__PURE__*/function () {\n  function RadioControler() {\n    _classCallCheck(this, RadioControler);\n  }\n\n  _createClass(RadioControler, [{\n    key: \"monitorForChange\",\n    value: function monitorForChange(radioControlContainerEl) {\n      radioControlContainerEl.querySelectorAll('input[type=radio]').forEach(function (elem) {\n        elem.addEventListener('change', function (event) {\n          var item = event.target.value;\n          var panelState = new _state_js__WEBPACK_IMPORTED_MODULE_0__.State();\n          panelState.deactivate(radioControlContainerEl.dataset.radioControl);\n          panelState.activate(\"[data-rc-panel-uid=\".concat(radioControlContainerEl.dataset.radioControl + '_' + item, \"]\"));\n        });\n      });\n    }\n  }, {\n    key: \"handleNestedRcRadios\",\n    value: function handleNestedRcRadios(nestedRadios) {\n      var selectedRadio = nestedRadios.querySelector('input[type=radio]:checked');\n      var defaultSelectedRadio = nestedRadios.querySelector('input[type=radio][data-rc-checked=true]');\n\n      if (nestedRadios.hasAttribute('data-rc-no-reset-nested-radios')) {\n        var nestedR = new _state_js__WEBPACK_IMPORTED_MODULE_0__.State();\n        nestedR.initiate(nestedRadios);\n      } else {\n        selectedRadio.checked = false;\n        defaultSelectedRadio.click();\n      }\n    }\n  }]);\n\n  return RadioControler;\n}();\n\n//# sourceURL=webpack://radio-control/./src/utils/radio_controler.js?");
+
+/***/ }),
+
+/***/ "./src/utils/state.js":
+/*!****************************!*\
+  !*** ./src/utils/state.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"State\": () => (/* binding */ State)\n/* harmony export */ });\n/* harmony import */ var _panel_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./panel.js */ \"./src/utils/panel.js\");\n/* harmony import */ var _radio_controler_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./radio_controler.js */ \"./src/utils/radio_controler.js\");\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\n\n\nvar State = /*#__PURE__*/function () {\n  function State() {\n    _classCallCheck(this, State);\n  }\n\n  _createClass(State, [{\n    key: \"initiate\",\n    value: function initiate(radioControlEl) {\n      var selectedRadioValue = radioControlEl.querySelector('input[type=radio]:checked').value;\n      var panelId = \"[data-rc-panel-uid=\".concat(radioControlEl.dataset.radioControl + '_' + selectedRadioValue, \"]\");\n      this.deactivate(radioControlEl.dataset.radioControl);\n      this.activate(panelId);\n    }\n  }, {\n    key: \"deactivate\",\n    value: function deactivate(panelId) {\n      var group = document.querySelector(\"[data-rc-panel-group=\".concat(panelId, \"]\"));\n      if (!group) return console.warn(\"Wrap your \".concat(panelId, \" pannels with a block-level element, and add the attribute: [data-rc-panel-group=\\\"\").concat(panelId, \"\\\"]\"));\n      group.querySelectorAll('[data-rc-panel-uid]').forEach(function (elem) {\n        var panel = new _panel_js__WEBPACK_IMPORTED_MODULE_0__.Panel();\n        panel.disable(elem);\n      });\n    }\n  }, {\n    key: \"activate\",\n    value: function activate(panelId) {\n      var targetPanel = document.querySelector(panelId);\n      if (!targetPanel) return console.warn(\"There is no panel in your DOM with the attribute: \\\"\".concat(panelId, \"\\\"\"));\n      var panel = new _panel_js__WEBPACK_IMPORTED_MODULE_0__.Panel();\n      panel.enable(targetPanel);\n      targetPanel.querySelectorAll('[data-radio-control]').forEach(function (nestedElement) {\n        var nestedRadios = new _radio_controler_js__WEBPACK_IMPORTED_MODULE_1__.RadioControler();\n        nestedRadios.handleNestedRcRadios(nestedElement);\n      });\n    }\n  }]);\n\n  return State;\n}();\n\n//# sourceURL=webpack://radio-control/./src/utils/state.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
